a1a90f68b4292b6b361652bb659f258963348f15
Deepak Devadathan <deepak.devadathan@oracle.com>
2024-05-30 00:34:06 +0000

    updated control center init
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- control-center/init.sh
+++ control-center/init.sh
@@ -2,6 +2,6 @@ echo "https://${PRIVATE_REPO_USER}:${PRIVATE_REPO_TOKEN}@${PRIVATE_REPO}" > ~/.g
 git config --global credential.helper 'store --file ~/.gitcredentials.store'
 git config --global advice.detachedHead false
 git clone $IAC_TERRAFORM_MODULES_REPO
-cd iac-devops
+cd govstack-sandbox-infra-oci
 git checkout $IAC_TERRAFORM_MODULES_TAG
 
