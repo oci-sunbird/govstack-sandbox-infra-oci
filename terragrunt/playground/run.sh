71aedd7a59e604618626b18d829b1f56e7de5796
Deepak Devadathan <deepak.devadathan@oracle.com>
2024-06-03 02:23:18 +0000

    updated run.sh
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- terragrunt/playground/run.sh
+++ terragrunt/playground/run.sh
@@ -3,3 +3,7 @@ cd sandbox-infra-playground
 rm -rf .terragrunt-cache
 terragrunt apply -auto-approve --terragrunt-non-interactive --target=module.vcn
 terragrunt apply -auto-approve --terragrunt-non-interactive
+
+cd ../sandbox-app-playground
+rm -rf .terragrunt-cache
+terragrunt apply -auto-approve --terragrunt-non-interactive
